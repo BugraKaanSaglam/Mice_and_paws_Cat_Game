9428b8401ff57b996292a269427d8da4377b8669
BugraKaanSaglam <bugrakaansaglam@gmail.com>
2024-02-24 12:09:01 +0300

    loading Screen Done

--- lib/functions/loading_screen_function.dart
+++ lib/functions/loading_screen_function.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:game_for_cats_flutter/main.dart';
 
-Center loadingScreen(BuildContext context) {
-  return Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(MainAppState().gameTheme.colorScheme.surface)),
-        const SizedBox(height: 20),
-        Text(AppLocalizations.of(context)!.loading, style: const TextStyle(color: Colors.white, fontSize: 24)),
-      ],
+Widget loadingScreen(BuildContext context) {
+  return Scaffold(
+    backgroundColor: MainAppState().gameTheme.colorScheme.background.withAlpha(200),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(child: CircularProgressIndicator(strokeWidth: 6, valueColor: AlwaysStoppedAnimation<Color>(MainAppState().gameTheme.colorScheme.surface))),
+          const SizedBox(height: 20),
+          Text(AppLocalizations.of(context)!.loading, style: const TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+        ],
+      ),
     ),
   );
 }
